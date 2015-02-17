3c7c51980e27007fadc2226e00df61f82aa62d46
SDLyu <dragon90321@gmail.com>
2015-02-17 17:14:43 +0800

    Add tooltips for team member.

--- js/recruitment.js
+++ js/recruitment.js
@@ -5,4 +5,14 @@ $(function(){
     speed: 'slow',
     effect: 'fade'
   });
+
+
+  $('.tooltips').tooltipster({
+    animation: 'fade',
+    delay: 200,
+    contentAsHTML: true,
+    theme: 'tooltipster-default',
+    touchDevices: false,
+    trigger: 'click'
+  });
 });
