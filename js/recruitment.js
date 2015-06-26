42daeae585f8a9e994be6ab2eb367932ec215476
SDLyu <dragon90321@gmail.com>
2015-06-26 17:29:16 +0800

    Update recuritment page.

--- js/recruitment.js
+++ js/recruitment.js
@@ -6,20 +6,21 @@ $(function(){
     effect: 'fade'
   });
 
+  $('.tooltips').popover({
+      html: true
+  });
 
-  $('.tooltips').tooltipster({
-    animation: 'fade',
-    delay: 200,
-    contentAsHTML: true,
-    theme: 'tooltipster-default',
-    touchDevices: false,
-    trigger: 'click',
-    maxWidth: 860,
-    autoClose: false,
-    hideOnClick: true
+  $(window).resize(function(){
+      if ($('.popover').is(":visible")) $('.popover').popover('show');
   });
 
   $('.team-member img').hover(function(){
       $(this).toggleClass('pulse animated');
+  });
+
+  $(window).resize(function(){
+      if ($('.popover').is(":visible")) {
+          $('.poper').popover('show');
+      }
   });
 });
